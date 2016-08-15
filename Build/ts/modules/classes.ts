ece11a93f0bab7a01218319951529f2d47b69f47
typicalmike002 <townsendm105@gmail.com>
2016-08-15 16:56:33 -0400

    Added SPajaxQuery class
    SPajaxQuery class uses the jQuery method of retriving items from a sharepoint2013 document library.  This class requires 2 arguments, the listName and the fields to retrieve from the list

--- Build/ts/modules/classes.ts
+++ Build/ts/modules/classes.ts
@@ -1,22 +1,28 @@
 /**
- * Class: camlQuery
+ * Class: SPcamlQuery
  *
- * SPCamlQuery(listName, query, onSuccess, onFail);
+ * SPCamlQuery(listName, query);
  *
  * Params:
  *     - listName(string): Name of the sharepoint document library to query.
  *     - query: The caml query to perform the opperation on.
+ *
+ * Notes: 
+ *     - Make sure sp.js is loaded before using this class.
  */
 
 export class SPCamlQuery {
     
     private query: string;
     private listName: string;
+    private onSuccess: Function;
+    private collListItem: any;
+    private clientContext: any;
   
     constructor(listName: string, query: string){
         this.listName = listName;
         this.query = query;
-    }
+    };
 
 
     /**
@@ -27,7 +33,7 @@ export class SPCamlQuery {
      */
 
     private onFail(sender: any, args: any){
-        console.log('The ' + this.listName + ' Query failed:\n' + 
+        console.log('The ' + this.listName + ' query failed:\n' + 
             args.get_message() + '\n' + args.get_stackTrace()
         );
     };
@@ -36,19 +42,15 @@ export class SPCamlQuery {
     /**
      * Method: getData
      *
-     * getData(onSuccess();
+     * getData(onSuccess);
      *
      * Params: 
-     *     - onSuccess(): callback function to execute on success.
+     *     - onSuccess: callback function to execute on success.
      *
      * Note:
      *     - SharePoint requires the callback function as well as
      *       clientContext and collListItem to be referenced by 'this'.
      */
-
-    private onSuccess: Function;
-    private collListItem: any;
-    private clientContext: any;
 
     public getData(onSuccess: Function) {
 
@@ -72,5 +74,69 @@ export class SPCamlQuery {
             Function.createDelegate(this, this.onSuccess),
             Function.createDelegate(this, this.onFail)
         );
-    }
+    };
+};
+
+
+
+/**
+ * Class: SPajaxQuery
+ *
+ * SPCamlQuery(listName, fields);
+ *
+ * Params:
+ *     - listName(string): Name of the sharepoint document library to query.
+ *     - fields: Array of values to pull from the listName.
+ *
+ * Notes: 
+ *     - Make sure sp.js is loaded before using this class.
+ *     - This class depends on the jQuery library.
+ */
+
+export class SPajaxQuery {
+
+    private listName: string;
+    private fields: string;
+    private onSuccess: Function;
+
+    constructor(listName: string, fields: string){
+        this.listName = listName;
+        this.fields = fields;
+    };
+
+    /**
+     * Method: onFail(errorMessage)
+     *
+     * - Logs error message if getData fails.
+     */
+
+    private onFail(errorMessage: string){
+        console.log('The ' + this.listName + ' query failed: ' + errorMessage);
+    };
+
+    /**
+     * Method: getData(onSuccess)
+     *
+     * Params:
+     * 
+     * - onSuccess: Function to return if ajax returns successfully.
+     */
+
+    public getData(onSuccess: Function){
+
+        // Attaches the callback function to the class:
+        this.onSuccess = onSuccess;
+
+        jQuery.ajax({
+            url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists/getByTitle("' + this.listName + '")/items',
+            type: 'GET',
+            headers: { "accept": "application/json;odata=verbose" },
+            success: function(){
+                return this.onSuccess();
+            },
+            error: function(error){
+                return this.onFail(error);
+            }
+        });
+    };
 };
